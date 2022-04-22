a29ab787fc89b4a5341535380fa2806187ae9a0c
Mohammad.s <mohammad.s@bugloos.com>
2022-04-22 12:53:10 +0430

    Initial commit

---
+++ src/scripts/main.js
@@ -0,0 +1,9 @@
+/* eslint-env browser */
+
+(function() {
+  document.addEventListener('DOMContentLoaded', function() {
+    console.log('heeelo');
+    gsap.to(body.current, {color: "#8c0"});
+  });
+
+})();
